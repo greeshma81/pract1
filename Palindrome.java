1a57a1b3412e381b86721605d2b08d28cf278e99
greeshma81 <annagoudgreeshmagoud@gmail.com>
2025-09-10 13:53:33 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string or number: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase(); // Remove spaces and lowercase
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
