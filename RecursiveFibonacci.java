4f261720a18359d64d16158963981c623885e4cd
greeshma81 <annagoudgreeshmagoud@gmail.com>
2025-09-10 14:21:16 +0530

    Add files via upload

---
+++ RecursiveFibonacci.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = scanner.nextInt();
+
+        System.out.print("Fibonacci Series: ");
+
